efe3ce19342029e0682031e4328443f45deab369
iborraxavi <50479651+iborraxavi@users.noreply.github.com>
2024-08-03 16:01:46 +0200

    Init project

---
+++ src/main/java/com/ibx/caixabank/caixabank_test/CaixabankTestApplication.java
@@ -0,0 +1,13 @@
+package com.ibx.caixabank.caixabank_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaixabankTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaixabankTestApplication.class, args);
+	}
+
+}
